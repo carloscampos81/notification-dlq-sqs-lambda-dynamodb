6200472605e2b0834832e338b2e3d43369ba7233
carlossilva.dextra <carlos.silva@dextra-sw.com>
2022-04-13 11:00:26 -0300

    files python

--- python-files/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-### [PROXIMA RELEASE]
-
-### Adiciona 10/02/2022
-- [CART_ADM-534](https://anbima.myjetbrains.com/youtrack/issue/CART_ADM-534) Provisionamento Lambda
--- python-files/package.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pip3 install -r requirements.txt --target ./package
-
-cd package
-zip -r ../my-deployment-package.zip .
-
-cd ..
-zip -r my-deployment-package.zip . -x package/\*
